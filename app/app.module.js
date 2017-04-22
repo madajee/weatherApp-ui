53615ae162efd23e8d58bd6a3554bd13d8ebe21b
madajee <MADAJEE@EMAIL.com>
2017-04-21 21:46:40 -0400

    second commit

--- app/app.module.js
+++ app/app.module.js
@@ -5,6 +5,24 @@ var app = angular.module('weatherApp', [
 ]);
 
 app.controller('weatherapiController', function($scope, $http) {
-    $http.get("https://gentle-dusk-58723.herokuapp.com/weatherapi/Atlanta").success(function(response) {$scope.data = response;});
-    //$http.get("http://rest-service.guides.spring.io/greeting").success(function(response) {$scope.todos = response;});
+    $scope.placeid = 1;
+    $scope.placeData = "";
+    $scope.cityname = "Atlanta";
+    $scope.citytemp = "";
+    $http.get("http://jsonplaceholder.typicode.com/todos/"+$scope.placeid).success(function(response) {$scope.placeData= response;});
+    $http.get("https://gentle-dusk-58723.herokuapp.com/weatherapi/"+$scope.cityname).success(function(response) {$scope.citytemp = response;});
+ 	
+ $scope.updatePlaceData = function (placeid) {
+        console.log("PlaceID" + placeid)
+        $scope.placeData = $http.get("http://jsonplaceholder.typicode.com/todos/"+placeid).success(function(response) {$scope.placeData= response;
+        return $scope.placeData;
+        });
+     };
+
+     $scope.updateCityTemp= function (cityname) {
+        console.log("CityName" + cityname)
+        $scope.citytemp = $http.get("https://gentle-dusk-58723.herokuapp.com/weatherapi/"+cityname).success(function(response) {$scope.citytemp = response;
+        return $scope.citytemp;
+        });
+     };     
  });
