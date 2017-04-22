e8a7a0c7153124b4d98f6dafeb0698221d9ae7c7
madajee <MADAJEE@EMAIL.com>
2017-04-22 17:32:20 -0400

    third commit

--- app/app.module.js
+++ app/app.module.js
@@ -5,19 +5,19 @@ var app = angular.module('weatherApp', [
 ]);
 
 app.controller('weatherapiController', function($scope, $http) {
-    $scope.placeid = 1;
-    $scope.placeData = "";
+   /* $scope.placeid = 1;
+    $scope.placeData = "";*/
     $scope.cityname = "Atlanta";
     $scope.citytemp = "";
-    $http.get("http://jsonplaceholder.typicode.com/todos/"+$scope.placeid).success(function(response) {$scope.placeData= response;});
+   // $http.get("https://jsonplaceholder.typicode.com/todos/"+$scope.placeid).success(function(response) {$scope.placeData= response;});
     $http.get("https://gentle-dusk-58723.herokuapp.com/weatherapi/"+$scope.cityname).success(function(response) {$scope.citytemp = response;});
  	
- $scope.updatePlaceData = function (placeid) {
+ /*$scope.updatePlaceData = function (placeid) {
         console.log("PlaceID" + placeid)
-        $scope.placeData = $http.get("http://jsonplaceholder.typicode.com/todos/"+placeid).success(function(response) {$scope.placeData= response;
+        $scope.placeData = $http.get("https://jsonplaceholder.typicode.com/todos/"+placeid).success(function(response) {$scope.placeData= response;
         return $scope.placeData;
         });
-     };
+     }; */
 
      $scope.updateCityTemp= function (cityname) {
         console.log("CityName" + cityname)
